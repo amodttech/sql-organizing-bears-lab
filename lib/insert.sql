792aefbb9fe21043998dd8347e7437db5569e881
Amodttech <aaron@amodttech.com>
2020-11-03 20:38:12 -0500

    mmmm SQL

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, NULL, NULL, NULL, NULL, 'Mad', TRUE);
+INSERT INTO bears VALUES (2, 'Mr. Chocolate', 3, 'M', 'green', 'sweet', TRUE);
+INSERT INTO bears VALUES (3, 'Rowdy', 9, 'M', 'grey', 'rowdy', TRUE);
+INSERT INTO bears VALUES (4, 'Tabitha', 12, 'F', 'brown', 'temp', TRUE);
+INSERT INTO bears VALUES (5, 'Sergeant Brown', 5, 'M', 'brown', 'temp', TRUE);
+INSERT INTO bears VALUES (6, 'Melissa', 4, 'F', 'color', 'red', TRUE);
+INSERT INTO bears VALUES (7, 'Grinch', 5, 'F', 'color', 'red', TRUE);
+INSERT INTO bears VALUES (8, 'Wendy', 8, 'F', 'color', 'grey', TRUE);
